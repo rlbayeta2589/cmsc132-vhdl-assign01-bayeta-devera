4cbd97668ef6e4cb4373a00b726dfb2a35497302
rlbayeta2589 <usefakename@gmail.com>
2016-03-25 19:46:51 +0800

    update script for EZ compile and run

--- script_buzz.sh
+++ script_buzz.sh
@@ -5,11 +5,11 @@ ghdl -a buzzer.vhdl
 echo "Executed : ghdl -a buzzer.vhdl"
 ghdl -e buzzer
 echo "Executed : ghdl -e buzzer"
-ghdl -a tb_buzzer.vhdl
-echo "Executed : ghdl -a tb_buzzer.vhdl"
-ghdl -e tb_buzzer
-echo "Executed : ghdl -e tb_buzzer"
-./tb_buzzer --vcd=buzz.vcd
-echo "Executed : ./tb_buzzer --vcd=buzz.vcd"
+ghdl -a buzzer_tb.vhdl
+echo "Executed : ghdl -a buzzer_tb.vhdl"
+ghdl -e buzzer_tb
+echo "Executed : ghdl -e buzzer_tb"
+./buzzer_tb --vcd=buzz.vcd
+echo "Executed : ./buzzer_tb --vcd=buzz.vcd"
 gtkwave buzz.vcd
 echo "Executed : gtkwave buzz.vcd"
