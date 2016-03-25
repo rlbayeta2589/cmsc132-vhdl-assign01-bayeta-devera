0d6a33ae0f41c17e536b9eac787b99f081b55db0
rlbayeta2589 <usefakename@gmail.com>
2016-03-25 19:58:22 +0800

    update script for EZ compile and run

--- script_buzz.sh
+++ script_buzz.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 # script for running Leceture Assignment
 
-ghdl -a buzzer.vhdl
-echo "Executed : ghdl -a buzzer.vhdl"
-ghdl -e buzzer
-echo "Executed : ghdl -e buzzer"
-ghdl -a buzzer_tb.vhdl
-echo "Executed : ghdl -a buzzer_tb.vhdl"
-ghdl -e buzzer_tb
-echo "Executed : ghdl -e buzzer_tb"
-./buzzer_tb --vcd=buzz.vcd
-echo "Executed : ./buzzer_tb --vcd=buzz.vcd"
-gtkwave buzz.vcd
+ghdl -a buzzer.vhdl &&
+echo "Executed : ghdl -a buzzer.vhdl"  &&
+ghdl -e buzzer &&
+echo "Executed : ghdl -e buzzer" &&
+ghdl -a buzzer_tb.vhdl &&
+echo "Executed : ghdl -a buzzer_tb.vhdl" &&
+ghdl -e buzzer_tb &&
+echo "Executed : ghdl -e buzzer_tb" &&
+./buzzer_tb --vcd=buzz.vcd &&
+echo "Executed : ./buzzer_tb --vcd=buzz.vcd" &&
+gtkwave buzz.vcd &&
 echo "Executed : gtkwave buzz.vcd"
